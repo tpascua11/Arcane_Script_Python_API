6defdbbda196fd76875c9362c7f6ac83e2ea610e
trpascua <ted.pascua.11@gmail.com>
2024-02-17 22:30:38 -0800

    Rewritten for basic postgressql connection and deployment for render

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask 
+
+from .extensions import db
+from .routes import main
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+    db.init_app(app)
+
+    app.register_blueprint(main)
+
+    return app
